1514495ca3c467c4e6804fed22ccfea3a775998e
Klimentiy Vervekin <klimwerwekin@gmail.com>
2025-12-17 11:44:58 +0100

    simple gamecard component done

--- components/GameCard.js
+++ components/GameCard.js
@@ -1,12 +1,34 @@
 import Link from "next/link";
+import Image from "next/image";
+import styled from "styled-components";
 
 export default function GameCard({ game }) {
   return (
     <Link href={`/games/${game.slug}`}>
-      <div className="card">
-        {game.coverUrl && <img src={game.coverUrl} alt={game.title} width="120" />}
-        <h3>{game.title}</h3>
-      </div>
+      <Card>
+        {game.coverUrl && (
+          <Image
+            src={game.coverUrl}
+            alt={game.title}
+            width={120}
+            height={90}
+          />
+        )}
+        <Title>{game.title}</Title>
+      </Card>
     </Link>
   );
 }
+
+const Card = styled.article`
+  display: flex;
+  gap: 12px;
+  padding: 12px;
+  border: 1px solid #ddd;
+  cursor: pointer;
+`;
+
+const Title = styled.h3`
+  margin: 0;
+`;
+
